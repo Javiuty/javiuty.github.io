89062ecb16f8d6814633e5773f55d309706f4894
Javiuty <javiiescuadra@gmail.com>
2021-01-22 09:08:33 +0100

    google analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -6,7 +6,14 @@ import Header from "./components/Header";
 import Info from "./components/Info";
 import Projects from "./components/Projects";
 
+import ReactGA from "react-ga";
+
 const App = () => {
+  // Google analytics
+  useEffect(() => {
+    ReactGA.initialize("181303345");
+  }, []);
+
   return (
     <>
       <header>
